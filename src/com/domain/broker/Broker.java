f22e7ec2d62df164d0dd217a1c49c7fcbc5e8ead
TONY <tonyalonzoc@gmail.com>
2015-10-03 20:37:05 -0500

    New Broker functions added.

--- src/com/domain/broker/Broker.java
+++ src/com/domain/broker/Broker.java
@@ -1,15 +1,53 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 
 package com.domain.broker;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 /**
- * 
- * @author Tony
+ *
+ * @author RamirezAlonzo
  */
 public class Broker {
-
+    private static Broker brokerInstance = null;
+    
+    private Broker() {
+        
+    }
+    
+    public static Broker getInstance() {
+        if (brokerInstance == null) {
+            brokerInstance = new Broker();
+        }
+        
+        return brokerInstance;
+    }
+    
+    private void beginConnection(int portNumber) {
+        boolean listening = true;
+        
+        try {
+            ServerSocket serverSocket = new ServerSocket(portNumber);
+            while (listening) {
+                new BrokerThread(serverSocket.accept()).start();
+            }
+        } catch (IOException e) {
+            System.err.println("Could not listen on port " + portNumber);
+        }
+    }
+    
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Usage: java Broker <port number>");
+        }
+        
+        int portNumber = Integer.parseInt(args[0]);
+        Broker broker = Broker.getInstance();
+        broker.beginConnection(portNumber);
+    }
 }
--- src/com/domain/broker/BrokerThread.java
+++ src/com/domain/broker/BrokerThread.java
@@ -1,15 +1,22 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 
 package com.domain.broker;
 
-/**
- * 
- * @author Tony
- */
-public class BrokerThread {
+import java.net.Socket;
 
+/**
+ *
+ * @author RamirezAlonzo
+ */
+public class BrokerThread extends Thread {
+    private Socket socket = null;
+    
+    public BrokerThread(Socket socket) {
+        super("KKMultiServerThread");
+        this.socket = socket;
+    }
 }
